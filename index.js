e7d28993b49e0a98a333306fd7429e1f2f444ee0
abul-hasan <abulhasan7bd@gmail.com>
2025-06-09 08:59:33 +0600

    single-blog

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const port = process.env.MY_PORT;
+const cors = require("cors");
 app.use(express.json());
-const { MongoClient, ServerApiVersion } = require("mongodb");
+app.use(cors());
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster11.spsqp4v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster11`;
 
 const client = new MongoClient(uri, {
@@ -19,7 +22,11 @@ async function run() {
     await client.connect();
     // SERVER NAME
     const blogServer = await client.db("blogServer");
-    const blogCollection = await blogServer.collection("blogCollection");
+    const blogCollection = blogServer.collection("blogCollection");
+    // await blogCollection.createIndex({ heading: "text" });
+    const wishListCollection = await blogServer.collection(
+      "wishListCollection"
+    );
     console.log("You successfully connected to MongoDB!");
 
     // 1 : RESPONSE ROUTE
@@ -38,6 +45,78 @@ async function run() {
     app.get("/all-blogs", async (req, res) => {
       const result = await blogCollection.find().toArray();
       res.send(result);
+    });
+
+    // SINGLE BLOG 
+    app.get("/singleblog/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await blogCollection.findOne(query);
+      res.send(result);
+    });
+    // 4 : WISH LIST ADD
+    app.post("/wishlist", async (req, res) => {
+      const wishListItem = req.body;
+      const result = await wishListCollection.insertOne(wishListItem);
+      res.status(201).send(result);
+    });
+
+    app.get("/all-wishlistPractice", async (req, res) => {
+      const result = await wishListCollection.find().toArray();
+      res.send(result);
+    });
+
+    // 5 : WISH LIST FIND BY USER
+    app.get("/all-wishlist", async (req, res) => {
+      const email = req.query.email;
+      const query = { wishListEmail: email };
+      const result = await wishListCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.delete("/wish-list-remove/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const result = await wishListCollection.deleteOne({
+        _id: id,
+      });
+      res.send(result);
+    });
+
+    app.put("/update/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateCoffee = req.body;
+      const updateDoc = {
+        $set: updateCoffee,
+      };
+      const result = await blogCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    // 6 : DELEATE ALL BLOG
+    app.delete("/deleteAllblog", async (req, res) => {
+      const result = await blogCollection.deleteMany();
+      res.send(result);
+    });
+    app.delete("/deleteAllWishList", async (req, res) => {
+      const result = await wishListCollection.deleteMany();
+      res.send(result);
+    });
+
+    // 7 : SEARCH IN MONGODB
+    app.get("/search", async (req, res) => {
+      const searchText = req.query.q;
+      try {
+        const result = await blogCollection
+          .find({ $text: { $search: searchText } })
+          .toArray();
+
+        res.send(result);
+      } catch (error) {
+        console.error("Search error:", error.message);
+        res.status(500).send({ message: "Search failed" });
+      }
     });
   } finally {
   }
